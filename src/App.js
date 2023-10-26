bf24b166e96a909f0741c9e477d02586fe2748b7
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-26 12:43:48 -0400

    made a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Car() {
+  return <h2>Hi, I am a Car!</h2>;
+}
+
+function X(){
+  return <p>rip xxxtentacion</p>
+}
+
 function App() {
   return (
     <div className="App">
               <img src={logo} className="App-logo" alt="logo" />
               <p>Hello React!</p>
-    </div>
+              <Car /><X/></div>
+    
   );
 }
 
